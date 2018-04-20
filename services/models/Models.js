b34998dd607292182aba24762fe80ed6ee2beaee
Toby <tobyberlinetta@gmail.com>
2018-04-20 21:15:12 +0800

    remove useless pages

--- services/models/Models.js
+++ services/models/Models.js
@@ -1,16 +1,16 @@
 const UserType = {
-  Anomymous: "0",
   Seller: "1",
   Dispatcher: "2",
   Driver: "3",
+  Anomymous: "4",
   Admin: "5"
 };
 
 const UserTypeName = {
-  [UserType.Anomymous]: "匿名用户",
   [UserType.Seller]: "销售员",
   [UserType.Dispatcher]: "调度员",
   [UserType.Driver]: "司机",
+  [UserType.Anomymous]: "匿名用户",
   [UserType.Admin]: "管理员"
 }; 
 
